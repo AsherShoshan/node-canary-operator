df8daad0264e104b77b43d266ab21ba59d6b54cf
root <root@cnv-qe-02.cnvqe.lab.eng.rdu2.redhat.com>
2019-08-14 19:44:08 +0300

    add deploy/undeploy scripts

---
+++ undeploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export TARGET_NAMESPACE="${TARGET_NAMESPACE:-openshift-operators}"
+
+for yml in operator.yaml  role_binding.yaml  role.yaml  service_account.yaml ; do
+      curl -k https://raw.githubusercontent.com/AsherShoshan/node-canary-operator/master/deploy/$yml | sed "s/TARGET_NAMESPACE/${TARGET_NAMESPACE}/g" | oc --namespace ${TARGET_NAMESPACE} delete -f -
+
+done
+  
